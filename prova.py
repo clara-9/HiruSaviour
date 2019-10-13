80e8fdd1c06832c947415d2a4c7b41d1137ec1a8
Gerard-CB <56447196+Gerard-CB@users.noreply.github.com>
2019-10-13 04:26:57 +0200

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,10 +1,10 @@
 import csv
 import pandas
 
-gener = pandas.read_csv('bo.csv',sep=',',error_bad_lines=False,low_memory=False).query("year>1995")
+ore = pandas.read_csv('bo.csv',sep=',',error_bad_lines=False,low_memory=False).query("year>1995")
 
 import plotly.express as px
-fig = px.scatter_geo(gener, lat="decimalLatitude", lon="decimalLongitude",
+fig = px.scatter_geo(ore, lat="decimalLatitude", lon="decimalLongitude",
                      animation_frame="month", color="year", projection = "natural earth")
       
 fig.show()
